f7c7b5b7fcaa052ce9426938c3398bd1243ca1c8
bianyc <bianyc1207@163.com>
2020-09-18 10:23:13 +0800

    新房源：保存房屋图片、房屋信息、设施

--- ihome/api_1_0/houses.py
+++ ihome/api_1_0/houses.py
@@ -3,10 +3,11 @@ import json
 from flask import current_app, jsonify, request, g
 from ihome import db, constants, redis_store
 from ihome.api_1_0 import api
-from ihome.models import Area
+from ihome.models import Area, House, Facility, HouseImage
 from ihome.utlis.response_code import RET
 from ihome.utlis.commons import login_required
 from ihome.utlis.image_storage import storage
+
 
 @api.route("/areas")
 def get_area_info():
@@ -44,3 +45,145 @@ def get_area_info():
         current_app.logger.error(e)
 
     return resp_json, 200, {"Content-Type": "application/json"}
+
+
+@api.route("/house/info", methods=["POST"])
+@login_required
+def save_house_info():
+    """保存房屋基本信息"""
+    # 获取数据
+    user_id = g.user_id
+    houses = request.get_data()
+    if not houses:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    house_data = json.loads(houses)
+
+    title = house_data.get("title")  # 房屋名称标题
+    price = house_data.get("price")  # 房屋单价
+    area_id = house_data.get("area_id")  # 房屋所属城区的编号
+    address = house_data.get("address")  # 房屋地址
+    room_count = house_data.get("room_count")  # 房屋包含的房间数目
+    acreage = house_data.get("acreage")  # 房屋面积
+    unit = house_data.get("unit")  # 房屋布局（几室几厅)
+    capacity = house_data.get("capacity")  # 房屋容纳人数
+    beds = house_data.get("beds")  # 房屋卧床数目
+    deposit = house_data.get("deposit")  # 押金
+    min_days = house_data.get("min_days")  # 最小入住天数
+    max_days = house_data.get("max_days")  # 最大入住天数
+
+    if not all([
+        title, price, area_id, address, room_count, acreage,
+        unit, capacity, beds, deposit, min_days, max_days
+    ]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不完整")
+
+    # 判断金额是否正确
+    try:
+        price = int(float(price) * 100)
+        deposit = int(float(deposit) * 100)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="房屋单价或者押金填写错误")
+
+    # 判断城区id是否存在
+    try:
+        area = Area.query.get(area_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询城区信息数据库异常")
+    if not area:
+        return jsonify(errno=RET.PARAMERR, errmsg="城区信息有误")
+
+    # 保存房屋信息
+    house = House(
+        user_id = user_id,
+        area_id = area_id,
+        title = title,
+        price = price,
+        address = address,
+        room_count = room_count,
+        acreage = acreage,
+        unit = unit,
+        capacity = capacity,
+        beds = beds,
+        deposit = deposit,
+        min_days = min_days,
+        max_days = max_days
+    )
+
+    # 处理房屋设施信息
+    facility_id_l = house_data.get("facility")
+    if facility_id_l:
+        try:
+            # sql: select * from table_name where fac_id in []
+            facilities = Facility.query.filter(Facility.fac_id.in_([int(i) for i in facility_id_l])).all()
+        except Exception as e:
+            current_app.logger.error(e)
+            return jsonify(errno=RET.DBERR, errmsg="查询设施信息数据库异常")
+        if facilities:
+            house.facilities = facilities
+
+    try:
+        db.session.add(house)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="保存房屋信息失败")
+
+    return jsonify(errno=RET.OK, errmsg="OK", data={"house_id": house.house_id})
+
+
+@api.route('/house/image', methods=["POST"])
+@login_required
+def save_house_image():
+    """
+    保存房屋图片信息
+    :param: image, house_id
+    :return:
+    """
+    image_file = request.files.get("house_image")
+    house_id = request.form.get("house_id")
+
+    if not all([image_file, house_id]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 判断house_id是否存在
+    try:
+        house = House.query.get(house_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询房屋信息错误")
+    if not house:
+        return jsonify(errno=RET.NODATA, errmsg="房屋不存在")
+
+    # 上传七牛云
+    img_data = image_file.read()
+    try:
+        image_name = storage(img_data)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg="上传图片失败")
+
+    # 保存图片信息到数据库
+    house_image = HouseImage(
+        house_id=house_id,
+        url=image_name
+    )
+    db.session.add(house_image)
+
+    # 设置房屋主图片
+    if not house.index_image_url:
+        house.index_image_url = image_name
+        db.session.add(house)
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="保存图片数据异常")
+
+    image_url = constants.QINIU_URL_DOMAIN + image_name
+
+    return jsonify(errno=RET.OK, errmsg="OK", data={"image_url": image_url})
+
--- ihome/static/js/ihome/newhouse.js
+++ ihome/static/js/ihome/newhouse.js
@@ -3,7 +3,93 @@ function getCookie(name) {
     return r ? r[1] : undefined;
 }
 
-$(document).ready(function(){
-    // $('.popup_con').fadeIn('fast');
-    // $('.popup_con').fadeOut('fast');
+$(document).ready(function () {
+    // 向后端发送请求获取城区信息
+    $.get("api/v1.0/areas", function (resp) {
+        if (resp.errno === "0") {
+            var areas = resp.data;
+            // for (i=0; i<areas.length; i++) {
+            //     var area = areas[i];
+            //     $("#area-id").append('<option value="'+ area.id +'">'+ area.name +'</option>');
+            // }
+
+            // 使用js模板
+            var html = template("area-tmp", {areas: areas})
+            $("#area-id").html(html);
+        } else {
+            alert(resp.errmsg);
+        }
+    }, "json");
+
+    $("#form-house-info").submit(function (e) {
+        e.preventDefault();
+
+        // 处理表单数据
+        var form_data = {};
+        $("#form-house-info").serializeArray().map(function (form) {
+            form_data[form.name] = form.value
+        });
+
+        // 收集设施id信息
+        var facilities = [];
+        $(":checked[name=facility]").each(function (index, value) {
+            facilities[index] = $(value).val();
+        });
+        form_data.facility = facilities;
+
+        // 向后端发送请求
+        var dataJson = JSON.stringify(form_data);
+        $.ajax({
+            url: "api/v1.0/house/info",
+            type: "post",
+            data: dataJson,
+            dataType: "json",
+            contentType: "application/json",
+            headers: {
+                "X-CSRFToken": getCookie("csrf_token")
+            },
+            success: function (resp) {
+                if (resp.errno === "4101") {
+                    // 用户未登录
+                    location.href = "/login.html";
+                } else if (resp.errno === "0") {
+                    // 隐藏基本信息表单
+                    $("#form-house-info").hide();
+                    // 显示图片表单
+                    $("#form-house-image").show();
+                    // 设置图片表单中的house_id
+                    $("#house-id").val(resp.data.house_id);
+                } else {
+                    alert(resp.errmsg);
+                }
+            }
+        })
+    });
+
+    $("#form-house-image").submit(function (x) {
+        // 阻止表单的默认行为
+        x.preventDefault();
+
+        // 利用jquery.form.min.js提供的ajaxSubmit对表单进行异步提交
+        $(this).ajaxSubmit({
+            url: "/api/v1.0/house/image",
+            type: "post",
+            dataType: "json",
+            headers: {
+                "X-CSRFToken": getCookie("csrf_token")
+            },
+            success: function (resp) {
+                if (resp.errno === "4101") {
+                    location.href = "/login.html";
+                } else if (resp.errno === "0") {
+                    var imageUrl = resp.data.image_url;
+                    $(".house-image-cons").append(
+                        '<img src="'+ imageUrl +'">'
+                    );
+                }else {
+                    alert(resp.errmsg);
+                }
+            }
+        })
+    });
 })
